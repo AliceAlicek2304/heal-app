271c6c44ed4297af42485d9e24532b27e35808cb
nguyen van cuong <cuongnvse183645@fpt.edu.vn>
2025-05-14 16:17:02 +0700

    hi

---
+++ src/main/java/com/healapp/HealAppApplication.java
@@ -0,0 +1,13 @@
+package com.healapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealAppApplication.class, args);
+	}
+
+}
