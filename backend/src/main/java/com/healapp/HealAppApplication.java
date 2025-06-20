839410fbca59034716fb4cd150b305d639401b8c
nguyen van cuong <cuongnvse183645@fpt.edu.vn>
2025-06-21 05:46:41 +0700

    8

--- backend/src/main/java/com/healapp/HealAppApplication.java
+++ backend/src/main/java/com/healapp/HealAppApplication.java
@@ -2,14 +2,18 @@ package com.healapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableAsync
+@EnableJpaRepositories("com.healapp.repository")
+@EntityScan("com.healapp.model")
 public class HealAppApplication {
 
 	public static void main(String[] args) {
